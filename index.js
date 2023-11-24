3c58fb4c425a99d5b954f6b9ba6f07024611a55d
Adnilec <holaadnilec@gmail.com>
2023-11-23 19:18:00 -0500

    Carga inicial

---
+++ index.js
@@ -0,0 +1,54 @@
+
+document.getElementById("Inicio1").style="display:none";
+document.getElementById("Acercade2").style="display:none";
+document.getElementById("Portafolio3").style="display:none";
+document.getElementById("Contacto4").style="display:none";   
+
+
+function btniraInicio () {
+
+    document.getElementById("Inicio1").style="display:normal"
+    document.getElementById("Acercade2").style="display:none";
+    document.getElementById("Portafolio3").style="display:none";
+    document.getElementById("Contacto4").style="display:none";
+} 
+
+function btniraAcercade () {
+
+    document.getElementById("Inicio1").style="display:none"
+    document.getElementById("Acercade2").style="display:normal";
+    document.getElementById("Portafolio3").style="display:none";
+    document.getElementById("Contacto4").style="display:none";
+}  
+function btniraPortafolio () {
+
+    document.getElementById("Inicio1").style="display:none"
+    document.getElementById("Acercade2").style="display:none";
+    document.getElementById("Portafolio3").style="display:normal";
+    document.getElementById("Contacto4").style="display:none";
+}  
+
+function btniraContacto () {
+
+    document.getElementById("Inicio1").style="display:none"
+    document.getElementById("Acercade2").style="display:none";
+    document.getElementById("Portafolio3").style="display:none";
+    document.getElementById("Contacto4").style="display:normal";
+}  
+
+
+function btnPrincipal(){
+    alert("Tu información ha sido enviada de manera satisfactoria, hugo tqm");
+
+    document.getElementById("Inicio1").style="display:normal";
+    document.getElementById("Acercade2").style="display:none";
+    document.getElementById("Portafolio3").style="display:none";
+    document.getElementById("Contacto4").style="display:none";
+
+}
+
+document.getElementById("ConciertoGrupales").style="display:normal";
+document.getElementById("Quinteto").style="display:normal";
+
+
+  
